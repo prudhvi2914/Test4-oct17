ad61fa06bf8c16a3662485136cf84f30d6f96373
prudhvi <prudhvi.satram1995@gmail.com>
2019-10-17 11:01:16 -0400

    player movement added

--- BabaIsYou-F19/GameScene.swift
+++ BabaIsYou-F19/GameScene.swift
@@ -11,8 +11,16 @@ import GameplayKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
 
+    // MARK: Outlets for sprites
+       var player:SKSpriteNode!
+       let PLAYER_SPEED:CGFloat = 20
+    
+    
+    
     override func didMove(to view: SKView) {
         self.physicsWorld.contactDelegate = self
+         self.player = self.childNode(withName: "baba") as! SKSpriteNode
+        
     }
    
     func didBegin(_ contact: SKPhysicsContact) {
@@ -24,6 +32,38 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // GET THE POSITION WHERE THE MOUSE WAS CLICKED
+              // ---------------------------------------------
+              let mouseTouch = touches.first
+              if (mouseTouch == nil) {
+                  return
+              }
+              let location = mouseTouch!.location(in: self)
+
+              // WHAT NODE DID THE PLAYER TOUCH
+              // ----------------------------------------------
+              let nodeTouched = atPoint(location).name
+              //print("Player touched: \(nodeTouched)")
+              
+              
+              // GAME LOGIC: Move player based on touch
+              if (nodeTouched == "upButton") {
+                  // move up
+                  self.player.position.y = self.player.position.y + PLAYER_SPEED
+              }
+              else if (nodeTouched == "downButton") {
+                  // move down
+                   self.player.position.y = self.player.position.y - PLAYER_SPEED
+              }
+              else if (nodeTouched == "leftButton") {
+                  // move left
+                   self.player.position.x = self.player.position.x - PLAYER_SPEED
+              }
+              else if (nodeTouched == "rightButton") {
+                  // move right
+                   self.player.position.x = self.player.position.x + PLAYER_SPEED
+              }
+        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
